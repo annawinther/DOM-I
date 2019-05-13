fe5ea591b7b27303a641b49ae4a400c0b0d0004e
Anna W <anski.anna@gmail.com>
2019-05-13 14:25:22 +0100

    nav bar added at the top of the page

--- js/index.js
+++ js/index.js
@@ -40,3 +40,56 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Task 1: Create selectors to point your data into elements
+// use IDs to update crs path content
+let callToAction = document.getElementById("cta-img");
+callToAction.setAttribute('src', 'img/header-img.png');
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', 'img/mid-page-accent.jpg');
+
+let topContent = document.querySelector('h1');
+topContent.textContent = "Innovation On Demand";
+
+let button = document.querySelector('button');
+button.textContent = "Get Started";
+//button.setAttribute('style', 'color: green');
+
+/*
+let nav = document.querySelectorAll("nav a");
+for (i = 0; i < nav.length; i++) {
+  nav[i].textContent = siteContent.nav[`nav-item-${i+1}`];
+}
+*/
+const navBar = document.querySelector('nav');
+const navLinks = navBar.querySelectorAll('a');
+const navObj = siteContent.nav;
+const navValues = Object.values(navObj);
+[...navLinks].forEach((link, index) => {
+  link.textContent = navValues[index];
+});
+
+navBar.style.color = "green";
+
+/*
+let navigation1 = document.querySelector("body > div > header > nav > a:nth-child(1)").textContent = "Home";
+let navigation2 = document.querySelector("body > div > header > nav > a:nth-child(2)").textContent = "About";
+let navigation3 = document.querySelector("body > div > header > nav > a:nth-child(3)").textContent = "Contact";
+let navigation4 = document.querySelector("body > div > header > nav > a:nth-child(4)").textContent = "Home";
+let navigation5 = document.querySelector("body > div > header > nav > a:nth-child(5)").textContent = "Home";
+let navigation6 = document.querySelector("body > div > header > nav > a:nth-child(6)").textContent = "Home";
+
+let nav = documnet.querySelector('nav');
+nav.style.color = "inherit";
+
+*/
+
+/*
+const paragraph = document.createElement('p');
+paragraph.textContent = 'This is a new paragraph';
+paragraph.id = 'fancyParagraph';
+paragraph.classList.add('cursive');
+const section = document.querySelector('section[main-content]');
+section.insertAdjacentElement('beforebegin', paragraph);
+*/
